0ca79f258e099e7b9019ce5296d4c167d4cb3b21
Antonio-Fir <62373111+Antonio-Fir@users.noreply.github.com>
2021-03-14 09:55:23 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var arriba = document.getElementById('arriba');
+var abajo = document.getElementById('abajo');
+var izquierda = document.getElementById('izquierda');
+var derecha = document.getElementById('derecha');
+
+function arribaa(){
+    arriba.style.height="100vh";
+}
+function arribaCerrar(){
+    arriba.style.height="0vh";
+}
+
+// abajo
+function abajoo(){
+    abajo.style.height="100vh";
+}
+function abajoCerrar(){
+    abajo.style.height="0vh";
+}
+
+// izquierda
+function izquierdaa(){
+    izquierda.style.width="100%";
+}
+function izquierdaCerrar(){
+    izquierda.style.width="0%";
+}
+
+// derecha 
+function derechaa(){
+    derecha.style.width="100%";
+}
+function derechaCerrar(){
+    derecha.style.width="0%";
+}
+
